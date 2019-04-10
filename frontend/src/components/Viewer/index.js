ee28e9c5505ae35cea1d84cb863359dd54a7d936
the_sociophobic <torikowashi@gmail.com>
2019-04-10 15:21:30 +0300

    blurred interface + resize unfinished

--- frontend/src/components/Viewer/index.js
+++ frontend/src/components/Viewer/index.js
@@ -4,18 +4,42 @@ import THREE from 'libs/three'
 export default class Viewer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      renderer: undefined,
+    }
     this.viewerRef = new React.createRef()
   }
 
+  updateDimensions() {
+    const ViewerDiv = this.viewerRef && this.viewerRef.current
+    const renderer = this.state.renderer
+    const camera = this.state.camera
+    if (!renderer || !ViewerDiv || !camera)
+      return
+    // camera.aspect = ViewerDiv.offsetWidth / ViewerDiv.offsetHeight
+    // camera.updateProjectionMatrix()
+    // this.setState({camera: camera})
+    // console.log(this.state.camera.aspect)
+    renderer.setSize(ViewerDiv.offsetWidth, ViewerDiv.offsetHeight)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions)
+  }
+
   componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions.bind(this))
+
     var ViewerDiv = this.viewerRef.current
 
     var scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x00ffff );
-    var camera = new THREE.OrthographicCamera(ViewerDiv.offsetWidth / -2, ViewerDiv.offsetWidth / 2, ViewerDiv.offsetHeight / -2, ViewerDiv.offsetHeight / 2, 1, 500);
+    scene.background = new THREE.Color( 0xffffff );
+    const camera = new THREE.OrthographicCamera(ViewerDiv.offsetWidth / -2, ViewerDiv.offsetWidth / 2, ViewerDiv.offsetHeight / -2, ViewerDiv.offsetHeight / 2, 1, 500);
+    this.setState({camera: camera})
     var controls = new THREE.OrbitControls( camera, ViewerDiv );
 
-    var renderer = new THREE.WebGLRenderer({ alpha: true });
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.setState({renderer: renderer})
     renderer.setClearColor( 0x000000, 0 );
     renderer.setSize(ViewerDiv.offsetWidth, ViewerDiv.offsetHeight );
     ViewerDiv.appendChild(renderer.domElement);
@@ -46,12 +70,17 @@ export default class Viewer extends Component {
 
     controls.update();
 
+    const that = this
     function animate() {
       requestAnimationFrame( animate );
-      renderer.render( scene, camera );
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.005;
-      // cube.position.z += 0.1;
+      renderer.render( scene, that.state.camera );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.005;
+      cube.position.z += 0.1;
+      const blurredCanvas = that.props.blurredCanvasRef.current
+      if (blurredCanvas)
+        blurredCanvas.getContext('2d').drawImage(renderer.domElement, 0, 0)
+        
       controls.update();
     }
     animate();
