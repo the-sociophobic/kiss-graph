413c42c99860e8698c3315372f2dee8085d87f08
the_sociophobic <torikowashi@gmail.com>
2019-07-10 10:17:03 +0300

    test mobile zoom

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -46,16 +46,16 @@ export default class Layout extends Component {
             {...props}
           />
         </div>
-        <div className="interface-container">
+        {/* <div className="interface-container">
           <div className="interface">
             {this.props.children}
-            {/* <textarea
+            <textarea
               value={this.state.retrievedData}
               rows={40}
               cols={20}
-            /> */}
+            />
           </div>
-        </div>
+        </div> */}
       </div>
     )
   }
