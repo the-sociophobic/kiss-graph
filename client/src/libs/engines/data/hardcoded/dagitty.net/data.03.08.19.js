48ba71920f3da2b2866e42548506c9a7c28d3525
the_sociophobic <torikowashi@gmail.com>
2019-08-03 20:19:52 +0300

    data upd + antialiasing optimisation

--- client/src/libs/engines/data/hardcoded/dagitty.net/data.03.08.19.js
+++ client/src/libs/engines/data/hardcoded/dagitty.net/data.03.08.19.js
@@ -1,3 +1,4 @@
+export default `
 Aelitta%20Ezugbaya E @0.038,1.039
 Agarkova%20Nastya E @0.487,-0.009
 Aglaya%20Lobantseva E @0.802,1.257
@@ -1539,3 +1540,4 @@ Zhenya%20Ivonin Liza%20Nikolaenko
 Zhenya%20Selicheva Dasha%20Solovyova
 Zhora Alina%20Zlobinskaya
 unknown1 Kostya%20Solovyov
+`
