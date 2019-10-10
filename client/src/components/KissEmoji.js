6ce4f7271d4a491608d16e9f009bd8d021fc4708
the_sociophobic <torikowashi@gmail.com>
2019-10-10 04:03:37 +0300

    HeatMap + ControlsHelp

---
+++ client/src/components/KissEmoji.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <span
+    className="kiss-emoji"
+    role="img"
+    aria-label="kiss"
+  >💋</span>
+)
---
+++ client/src/components/interface/HeatMap.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import StoreContext from 'libs/engines/data/store/StoreContext'
+import heatMap from 'img/heat4.png'
+import KissEmoji from 'components/KissEmoji'
+
+export default class HeatMap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  static contextType = StoreContext
+
+  render = () => {
+    const { weightSet } = this.context.store.get()
+
+    return (
+      <div className="heat-map">
+        <img src={heatMap} alt="heatMap.png" />
+        <div className="heat-map__0">{weightSet[weightSet.length - 1]}</div>
+        <div className="heat-map__1">{weightSet[Math.round((weightSet.length - 1) / 4 * 3)]}</div>
+        <div className="heat-map__2">{weightSet[Math.round((weightSet.length - 1) / 2)]}</div>
+        <div className="heat-map__3">{weightSet[Math.round((weightSet.length - 1) / 4)]}</div>
+        <div className="heat-map__4">{weightSet[0]}</div>
+        <div className="heat-map__description">
+          number of<KissEmoji />
+        </div>
+      </div>
+    )
+  }
+}
