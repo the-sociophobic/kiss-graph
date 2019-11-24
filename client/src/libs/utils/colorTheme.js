3f6766b2ac20e85cbf56e98c3a462c8a283ff892
the_sociophobic <torikowashi@gmail.com>
2019-11-24 19:46:11 +0300

    matchMedia.addEventListener() Safari error fix

--- client/src/libs/utils/colorTheme.js
+++ client/src/libs/utils/colorTheme.js
@@ -1,16 +1,21 @@
-const currentTheme = () =>
+const accessor =
   window.matchMedia &&
-  window.matchMedia('(prefers-color-scheme: dark)').matches
+  (window.matchMedia('(prefers-color-scheme: dark)') ||
+  window.matchMedia('(prefers-color-scheme: light)')) &&
+  (window.matchMedia('(prefers-color-scheme: dark)') ||
+  window.matchMedia('(prefers-color-scheme: light)'))
+
+const currentTheme = () =>
+  accessor.matches
   ? "dark" : "light"
 
 const addThemeEventListener = fn =>
-  window.matchMedia &&
-  window.matchMedia('(prefers-color-scheme: dark)')
+  //somehow Safari doesn't have .addEventListener, but event still fires
+  accessor && accessor.addEventListener && accessor
   .addEventListener("change", fn)
 
 const removeThemeEventListener = fn =>
-  window.matchMedia &&
-  window.matchMedia('(prefers-color-scheme: dark)')
+  accessor && accessor.removeEventListener && accessor
   .removeEventListener("change", fn)
 
 export {
