44c36207d0fc383caaaa5a3c7408123bdbd50010
the_sociophobic <torikowashi@gmail.com>
2020-05-04 23:50:47 +0300

    yandex video rec + upd data

--- client/src/components/Helmet/config.js
+++ client/src/components/Helmet/config.js
@@ -37,13 +37,13 @@ if (isProduction()) {
   // `;
   // document.head.appendChild(googleAddSense);
 
-  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  // (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  // m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  // (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-  ym(54942766, "init", {
-      clickmap:true,
-      trackLinks:true,
-      accurateTrackBounce:true
-  });
+  // ym(54942766, "init", {
+  //     clickmap:true,
+  //     trackLinks:true,
+  //     accurateTrackBounce:true
+  // });
 }
