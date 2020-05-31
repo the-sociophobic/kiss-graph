563c8d0e902e9b4bd8c0420743bf1c6bfab94d29
the_sociophobic <torikowashi@gmail.com>
2020-05-31 05:05:37 +0300

    floaat positions

--- client/src/libs/engines/data/store/storeClass.js
+++ client/src/libs/engines/data/store/storeClass.js
@@ -220,9 +220,11 @@ class store extends listenersClass {
     edges: this.metaData.edges,
   }))
   //copy buffer can't handle that size
-  copyPos = () => copyToClipboard(JSON.stringify(
-    this.geometry.position
-  ))
+  copyPos = () => {
+    console.log(this.geometry.position.map(coordinate => parseFloat(coordinate.toFixed(7))))
+    copyToClipboard(JSON.stringify(
+    this.geometry.position.map(coordinate => parseFloat(coordinate.toFixed(7)))
+  ))}
   copyUV = () => copyToClipboard(JSON.stringify(
     this.geometry.uv
   ))
