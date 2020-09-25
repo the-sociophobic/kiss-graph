3eda00bef0fde99251e94b19db4f3f4374ab5897
Lev Vasilyev <torikowashi@gmail.com>
2020-09-25 12:42:56 +0300

    nebdt static files

--- client/app.js
+++ client/app.js
@@ -27,6 +27,7 @@ app.get('/libs/recycle-polytech/icon.png', (req, res) =>
 
 
 app.use('/ar', express.static('libs/ar'))
+app.use('/nebdt', express.static('libs/nebdt'))
 
 
 app.get('/data/nodes', (req, res) =>
