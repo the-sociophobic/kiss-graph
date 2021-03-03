fe3b04b5c3a1299e767affa9c325777ad7336d32
sociophobic <torikowashi@gmail.com>
2021-03-03 21:46:33 +0300

    change data axios to localhost

--- client/src/libs/engines/data/store/storeClass.js
+++ client/src/libs/engines/data/store/storeClass.js
@@ -49,10 +49,10 @@ class store extends listenersClass {
 
   getData = async () => {
     if (isProduction()) {
-      this.metaData = (await axios.get("https://kiss-graph.com/data/nodes")).data
+      this.metaData = (await axios.get("http://localhost:3005/data/nodes")).data
       this.geometry = {
-        position: (await axios.get("https://kiss-graph.com/data/position")).data,
-        uv: (await axios.get("https://kiss-graph.com/data/uv")).data,
+        position: (await axios.get("http://localhost:3005/data/position")).data,
+        uv: (await axios.get("http://localhost:3005/data/uv")).data,
       }
     } else {
       this.metaData = edgesTypes({ //REDO THIS SHIT
